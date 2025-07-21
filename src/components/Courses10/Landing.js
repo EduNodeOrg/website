907f5c70accbe38490912dade740f00df4533285
olvisgil <olvisgil@gmail.com>
2025-07-21 22:41:57 +0200

    courses fix

---
+++ src/components/Courses10/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NavBar from '../NavBar';
+import Footer from '../Footer';
+
+export default class Landing extends Component {
+    render() {
+        return (
+          <div>
+            <NavBar />
+          </div>
+        );
+    }
+}
---
+++ src/components/Courses10/One/Intro.js
@@ -0,0 +1,286 @@
+import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import LinearProgress from '@mui/material/LinearProgress';
+//import ProgressBar from 'react-bootstrap/ProgressBar';
+import Box from '@mui/material/Box';
+import NavBar from '../../NavBar/index.js';
+import Navbar from '../../Dashboard/Navbar1.js';
+
+import { connect } from 'react-redux';
+import { clearErrors } from '../../../actions/errorActions.js';
+import { reduxForm } from 'redux-form';
+import { useNavigate } from 'react-router-dom';
+import "./styles.css"
+import Precourse from "../Precourse.js"
+import { Container } from '@mui/material';
+import ReactPlayer from 'react-player'
+
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  resetContainer: {
+    padding: theme.spacing(3),
+  },
+}));
+
+function getSteps() {
+  return [
+    'Sesión 1: Introducción a Blockchain y Web3 - Profesora: Brenda Hidalgo',
+    'Sesión 2: Wallets y Criptomonedas - Profesora: Briguit Reinaldo:',
+    'Sesión 3: Infraestructura Blockchain - Profesores: Tomasso Di Ponte y Alberto Chaves',
+    'Sesión 4: Finanzas Descentralizadas y Tokens - Profesor: Lenin Tarrillo',
+    'Sesión 5: NFTs, DAOs y Gobernanza - Profesores: Nxsh y Lilian Santos',
+  ];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return (
+        <div>
+          <ReactPlayer url='https://youtu.be/W-VOT283j_Y' />
+        </div>
+      );
+    case 1:
+      return (
+        <div>
+          <ReactPlayer url='https://youtu.be/QeKo3Ph9m5c' />
+        </div>
+      );
+    case 2:
+      return (
+        <div>
+
+          <ReactPlayer url='https://youtu.be/CepfexoB1ZE' />
+
+
+        </div>
+      );
+    case 3:
+      return (
+        <div>
+          <ReactPlayer url='https://youtu.be/1sVZhezdQfo' />
+        </div>
+      )
+    case 4:
+      return (
+        <div>
+          <ReactPlayer url='https://youtu.be/1sVZhezdQfo' />
+        </div>
+      );
+    default:
+      return 'Unknown step';
+  }
+}
+
+export function VerticalLinearStepper(props) {
+  const classes = useStyles();
+
+  const steps = getSteps();
+  const { activeStep, handleNext, handleBack } = props;
+
+
+
+  return (
+    <>
+      <Precourse />
+      <div className={classes.root} >
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+              <StepContent>
+                <Typography>{getStepContent(index)}</Typography>
+                <div className={classes.actionsContainer}>
+                  <div>
+                    <Button
+                      type="button"
+                      label="back"
+                      id="back"
+                      disabled={activeStep === 0}
+                      onClick={handleBack}
+                      className={classes.button}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1
+                        ? 'Finish'
+                        : 'Next'}
+                    </Button>
+                  </div>
+                </div>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper
+            square
+            elevation={0}
+            className={classes.resetContainer}
+          >
+            <Typography>
+              Course completed - Now get ready for the
+              Quiz.
+            </Typography>
+            <Link to="/courses/110/1" className={classes.button}>
+              Continue
+            </Link>
+          </Paper>
+        )}
+      </div>
+    </>
+  );
+
+}
+
+
+
+
+
+class Intro extends Component {
+  navigateTo = () => {
+    const navigate = useNavigate();
+    navigate('/courses/110');
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: 0,
+      activeStep: 0,
+      activeProgress: 0,
+      stepone: 10,
+      isLoading: false,
+      errors: {},
+    };
+    this.onChangeOne = this.onChange.bind(this);
+  }
+
+
+  handleNext = () => {
+    this.setState((prevState) => ({
+      activeStep: prevState.activeStep + 1,
+      activeProgress: prevState.activeProgress + 10,
+    }));
+  };
+
+  handleBack = () => {
+    this.setState((prevState) => ({
+      activeStep: prevState.activeStep - 1,
+      activeProgress: prevState.activeProgress - 10,
+    }));
+  };
+
+  onChange = (e) => {
+    // e.preventDefault();
+    console.log('Please select a value');
+  };
+
+  render(props, state) {
+    const { activeStep, activeProgress } = this.state;
+    const progress = this.state.progress
+    return (
+      <div>
+        <Navbar></Navbar>
+        <LinearProgressWithLabel value={activeProgress} />
+        <VerticalLinearStepper
+          activeStep={activeStep}
+          handleNext={this.handleNext}
+          handleBack={this.handleBack}
+        />
+        
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+});
+
+Intro = connect(mapStateToProps, { clearErrors })(Intro);
+
+export default Intro = reduxForm({
+  form: '',
+  fields: [''],
+  clearErrors,
+
+})(Intro);
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box display="flex" alignItems="center">
+      <Box width="100%" mr={1}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box minWidth={35}>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+        >{`${Math.round(props.value)}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
+
+// const useStyles = makeStyles({
+//   root: {
+//     width: '100%',
+//   },
+// });
+
+function LinearWithValueLabel(props) {
+  const classes = useStyles();
+  const [progress, setProgress] = React.useState(props.props);
+
+  React.useEffect(() => {
+    //  setProgress(prev => prev + 10)
+  }, []);
+  // const now = 60;
+
+  return (
+    <div className={classes.root}>
+
+
+      {/* <ProgressBar now={now} label={`${now}%`} /> */}
+
+    </div>
+  );
+}
